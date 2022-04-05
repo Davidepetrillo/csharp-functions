fde0834574b593d5af68308e0e7860dc2023c312
davidepetrillo <davide96petrillo@gmail.com>
2022-04-05 15:53:53 +0200

    commit 1 - aggiunta funzione StampaArray

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿// ---------- Funzioni ----------
+
+void StampaArray(int[] array)
+{
+    int ultimoIndiceArray = array.Length - 1;
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == ultimoIndiceArray)
+        {
+            Console.Write(array[i]);
+        }
+        else
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int Quadrato(int numero)
+{
+
+}
+
+
+// ---------- Programma ----------
+
+int[] arrayDiNumeri = {2, 6, 7, 5, 3, 9};
+StampaArray(arrayDiNumeri);
+
+
